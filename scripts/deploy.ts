10b2f7c41d221d88f7d77e9e96fbd922aed7dcfb
Vladislav <81467635+grGred@users.noreply.github.com>
2022-07-29 16:24:30 +0300

    tests

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,22 +6,16 @@ const hre = require("hardhat");
 
 async function main() {
   const tokenFactory = await ethers.getContractFactory("TestERC20");
-  // const token1 = (await tokenFactory.deploy()) as TestERC20;
-  //
-  // console.log("Token deployed to:", token1.address);
-  //
-  // await new Promise((r) => setTimeout(r, 10000));
-  //
-  // const veNFTFactory = await ethers.getContractFactory("contracts/ve.sol:ve");
-  // const veNFT = (await veNFTFactory.deploy(token1.address)) as Ve;
-  //
-  // console.log("Ve deployed to:", veNFT.address);
-  //
-  // await new Promise((r) => setTimeout(r, 10000));
+  const veNFTFactory = await ethers.getContractFactory("contracts/ve.sol:ve");
+  const veNFT = (await veNFTFactory.deploy('0xd452d01C6348D3d5B35FA1d5500d23F8Ae65D6eA')) as Ve;
+
+  console.log("Ve deployed to:", veNFT.address);
+
+  await new Promise((r) => setTimeout(r, 10000));
 
   const RewardFactory = await ethers.getContractFactory("Reward");
   const reward1 = (await RewardFactory.deploy(
-    '0x3BBF11E07cE979769da5f263Cb4f66dC88B5bBea',
+    veNFT.address,
     '0xd452d01C6348D3d5B35FA1d5500d23F8Ae65D6eA'
   )) as Reward;
 
@@ -30,20 +24,15 @@ async function main() {
   // eslint-disable-next-line promise/param-names
   await new Promise((r) => setTimeout(r, 10000));
 
-  // await hre.run("verify:verify", {
-  //   address: '0xd452d01C6348D3d5B35FA1d5500d23F8Ae65D6eA',
-  //   constructorArguments: [],
-  // });
-  //
-  // await hre.run("verify:verify", {
-  //   address: veNFT.address,
-  //   constructorArguments: [token1.address],
-  // });
+  await hre.run("verify:verify", {
+    address: veNFT.address,
+    constructorArguments: ['0xd452d01C6348D3d5B35FA1d5500d23F8Ae65D6eA'],
+  });
 
   await hre.run("verify:verify", {
     address: reward1.address,
     constructorArguments: [
-        '0x3BBF11E07cE979769da5f263Cb4f66dC88B5bBea',
+        veNFT.address,
         '0xd452d01C6348D3d5B35FA1d5500d23F8Ae65D6eA'
   ],
   });
@@ -52,11 +41,12 @@ async function main() {
       '0xd452d01C6348D3d5B35FA1d5500d23F8Ae65D6eA'
     )) as TestERC20;
 
+  await token.mint(reward1.address, Web3.utils.toWei("10000000", "ether")); // 100 mil
+
   // eslint-disable-next-line promise/param-names
   await new Promise((r) => setTimeout(r, 10000));
 
-  await token.mint(reward1.address, Web3.utils.toWei("10000000", "ether")); // 100 mil
-  // await token1.approve(veNFT.address, Web3.utils.toWei("100000000", "ether"));
+  await token.approve(veNFT.address, Web3.utils.toWei("100000000", "ether"));
 
   // eslint-disable-next-line promise/param-names
   await new Promise((r) => setTimeout(r, 10000));
@@ -65,11 +55,13 @@ async function main() {
   const block = await ethers.provider.getBlock(blockNum);
   const timestamp = block.timestamp;
 
-  const week = 604800; // 1 epoch
+  const week = 300; // 1 epoch
+  // await reward1.addEpoch(timestamp+60*20, timestamp+week+60*20, Web3.utils.toWei('100000', 'ether'));
+
   await reward1.addEpochBatch(
     timestamp,
     week,
-    4,
+    50,
     Web3.utils.toWei("100000", "ether")
   );
 }
